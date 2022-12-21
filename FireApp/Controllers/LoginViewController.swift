0101ef396e1e6df8344fc82e335853b2796c5ce6
Ihor <56516292+degster55@users.noreply.github.com>
2022-12-21 12:58:26 +0200

    imported Firebase, worked with show and hide keyboard

--- FireApp/Controllers/LoginViewController.swift
+++ FireApp/Controllers/LoginViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-
+    
     @IBOutlet weak var warningLabel: UILabel!
     @IBOutlet weak var emailTexField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
@@ -17,9 +17,22 @@ class LoginViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        NotificationCenter.default.addObserver(self, selector: #selector(kbDidShow), name: UIResponder.keyboardDidShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(kbDidHide), name: UIResponder.keyboardDidHideNotification, object: nil)
     }
-
-
+    
+    @objc func kbDidShow(notification: Notification) {
+        guard let userInfo = notification.userInfo else { return }
+        let kbFrameSize = (userInfo[UIResponder.keyboardFrameEndUserInfoKey] as! NSValue).cgRectValue
+        (self.view as! UIScrollView).contentSize = CGSize(width: self.view.bounds.size.width, height: self.view.bounds.size.height + kbFrameSize.height)
+        
+        (self.view as! UIScrollView).scrollIndicatorInsets = UIEdgeInsets(top: 0, left: 0, bottom: kbFrameSize.height, right: 0)
+    }
+    
+    @objc func kbDidHide() {
+        (self.view as! UIScrollView).contentSize = CGSize(width: self.view.bounds.size.width, height: self.view.bounds.size.height)
+    }
+    
     @IBAction func loginTap(_ sender: UIButton) {
     }
     
